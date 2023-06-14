0f799fbb20a2da1241c72e6ae4eec5ef9841f4ca
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-06-14 07:59:38 +0600

    first components done

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,63 @@
-import './App.css'
-import FirstComponent from './components/FirstComponent'
+import "./App.css";
+import FirstComponent from "./components/FirstComponent";
 
 function App() {
-  const users=[
+  const users = [
     {
-      name:'Raj',
-      class:10,
-      occupation:'student'
+      name: "Raj",
+      class: 10,
+      occupation: "student",
     },
     {
-      name:'Rohan',
-      class:12,
-      occupation:'student'
+      name: "Rohan",
+      class: 12,
+      occupation: "student",
     },
     {
-      name:'Abhisheikh',
-      class:9,
-      occupation:'student'
+      name: "Abhisheikh",
+      class: 9,
+      occupation: "student",
     },
     {
-      name:'Rokib',
-      class:13,
-      occupation:'student'
+      name: "Rokib",
+      class: 13,
+      occupation: "student",
     },
-  ]
+  ];
 
   return (
     <>
-     <h1>Users list is displayed.</h1>
-     {
-      users.map(user=><FirstComponent key={user.class} user={user}/>)
-     }
+      <div className="flex items-center justify-center">
+        <h1 className="font-bold text-3xl">Users list is displayed.</h1>
+      </div>
+
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Id
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Class
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Occupation
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user,i) => (
+              <FirstComponent key={user.class} i={i} user={user} />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/FirstComponent.jsx
+++ src/components/FirstComponent.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
-function FirstComponent() {
+function FirstComponent({user,i}) {
   return (
-    <div>FirstComponent</div>
+    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {i+1}
+                </th>
+                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {user.name}
+                </th>
+                <td className="px-6 py-4">
+                    {user.class}
+                </td>
+                <td className="px-6 py-4">
+                    {user.occupation}
+                </td>
+            </tr>
   )
 }
 
